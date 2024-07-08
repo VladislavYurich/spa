85b800145488815d4aa2183796eaf78f5310657b
VladislavYurich <vlad_69rus@mail.ru>
2024-07-08 15:26:26 +0300

    add files

---
+++ js/newMap.js
@@ -0,0 +1,33 @@
+
+const mapNavBtn = document.querySelector('a[href="./map"]')
+
+mapNavBtn.addEventListener('click', function() {
+    setTimeout(function() {
+        ymaps.ready(init);
+        function init(){
+            // Создание карты.
+            var myMap = new ymaps.Map("map", {
+                // Координаты центра карты.
+                // Порядок по умолчанию: «широта, долгота».
+                // Чтобы не определять координаты центра карты вручную,
+                // воспользуйтесь инструментом Определение координат.
+                center: [56.736344, 37.162185],
+                // Уровень масштабирования. Допустимые значения:
+                // от 0 (весь мир) до 19.
+                zoom: 13
+            });
+    
+            var myPlacemark = new ymaps.Placemark([56.736344, 37.162185], {}, {
+                iconLayout: 'default#image',
+                iconImageHref: './img/pin.svg',
+                iconImageSize: [30, 42],
+                iconImageOffset: [-3, -42]
+            });
+    
+    
+            // Размещение геообъекта на карте.
+            myMap.geoObjects.add(myPlacemark);
+        }
+    }, 200)
+})
+
